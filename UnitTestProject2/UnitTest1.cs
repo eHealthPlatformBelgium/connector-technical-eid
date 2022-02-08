a3f8cf5c72b934c32fd1a4ca0dc5658d1f569648
Bob Walbers <b.walbers@farmad.be>
2022-02-08 15:47:40 +0100

    add unit test

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -9,13 +9,23 @@ namespace UnitTestProject2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_signature()
         {
             Console.WriteLine("start test");
             var proxy = new BeidPKCS11Proxy();
             byte[] testdata = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-            byte[] signeddata = proxy.SignData(testdata, "SHA-256", Alias.AUTHENTICATION );
+            byte[] signeddata = proxy.SignData(testdata, "SHA-256", Alias.AUTHENTICATION);
             Assert.IsNotNull(signeddata);
+            Console.WriteLine("test ended");
+        }
+
+        [TestMethod]
+        public void Test_getAlgorithm()
+        {
+            Console.WriteLine("start test");
+            var proxy = new BeidPKCS11Proxy();
+            var algo = proxy.GetAlgorithm(Alias.AUTHENTICATION);
+            Console.WriteLine("Algorithm: {0}", algo.ToString());
             Console.WriteLine("test ended");
         }
     }
