af7fcef0c0b04d6f23a1d3d10b1dca7b00e94c46
Bob Walbers <b.walbers@farmad.be>
2022-02-08 13:28:45 +0100

    add unit test project

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using connector_technical_eid;
+
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("start test");
+            var proxy = new BeidPKCS11Proxy();
+            byte[] testdata = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            byte[] signeddata = proxy.SignData(testdata, "SHA-256", Alias.AUTHENTICATION );
+            Assert.IsNotNull(signeddata);
+            Console.WriteLine("test ended");
+        }
+    }
+}
